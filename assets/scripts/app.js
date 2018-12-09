6b4bca04ec90c8be952ed95be8d27439fffb2b8c
Jaurs <rhsemail01@gmail.com>
2018-12-08 20:58:31 -0500

    Progress as of 12/8/2018

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -4,21 +4,36 @@ $(document).ready(function() {
     var term = $("#term").val();
     console.log(term);
 
-    var apiParams = {action: 'opensearch', search: term , limit: 10, namespace: 0, formatversion: 1, format: 'json'};
+    var apiParams = {
+      action: "opensearch",
+      search: term,
+      limit: 10,
+      namespace: 0,
+      formatversion: 1,
+      format: "json"
+    };
     var api = "https://en.wikipedia.org/w/api.php?" + $.param(apiParams);
 
     $.ajax({
       url: api,
-      dataType: 'jsonp',
-      success: function(data){
-        console.log(data);
-        console.log(data);
-        for(i = 0; i < data.length; i++) {
-          if(data.length > 0) {
-          $("#result").html("<br>" + "<div class='card' id='data'>" + data[2][0]+ "</div>");
-        }
-        }
+      dataType: "jsonp",
+      success: function(data) {
+        let header = data[1];
+        let desc = data[2];
+        let link= data[3];
         
+
+        var com=header.map(function(itm,i){
+          return ["<div class='card'><div class='card-body'>" +"<a style='text-decoration: none' href=" + link + ">" + "<h5>" + itm + "</h5>" + desc[i] + "</a></div></div>"];
+        }).join('<br>');
+
+
+        $('#result').append("<div class='card-body'>" + com + '</div></div>');
+
+        console.log(header);
+        console.log(desc);
+        console.log(link);
+
 
       }
     });
